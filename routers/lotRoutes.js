471eb1e6a865c740bb27935de91dc7ed4b0e0f6b
adanni3 <akanjii040@gmail.com>
2025-06-16 21:18:50 +0100

    testing esp2

--- routers/lotRoutes.js
+++ routers/lotRoutes.js
@@ -15,7 +15,7 @@ lotrouter.post(
     console.log("--- DEBUGGING MULTER PARSING ---");
     console.log("req.file:", req.file); // Should contain details about the image
     console.log("req.body:", req.body); // Should contain lotName and occupied
-    console.log("---------------------------------");}
+    console.log("---------------------------------");},
   validate(schema.parkingSchema, "body"), 
   lotController.lotpost
 );
