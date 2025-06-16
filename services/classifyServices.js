f4373b161b10771334e096c2f5417f0ba49f3f81
adanni3 <akanjii040@gmail.com>
2025-06-16 05:28:22 +0100

    Fix conflicting names2

--- services/classifyServices.js
+++ services/classifyServices.js
@@ -1,5 +1,5 @@
 const sharp     = require('sharp');        // still used for cropping / resizing
-const logger    = require('../utils/logger');
+const logger    = require('../../utils/logger');
 
 
 /* LOAD THE WASM MODELS ONCE AT START-UP*/
@@ -8,8 +8,8 @@ const logger    = require('../utils/logger');
 async function classifyImage (fullImagePath, lotName) {
  try {
   const edgeImpulseModel = {
-    lotA: require('../wasm-models/lotA-model/run-impulse'),
-    lotB: require('../wasm-models/lotB-model/run-impulse'),
+    lotA: require('../../wasm-models/lotA-model/run-impulse'),
+    lotB: require('../../wasm-models/lotB-model/run-impulse'),
     /* add more lots here … */
   };
    
