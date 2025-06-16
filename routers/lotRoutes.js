4e2904ecae95eb63865b78b1a6dd1e6ce1eca155
adanni3 <akanjii040@gmail.com>
2025-06-16 21:11:15 +0100

    testing esp

--- routers/lotRoutes.js
+++ routers/lotRoutes.js
@@ -10,7 +10,12 @@ const middleware = require("../utils/middleware");
 
 lotrouter.post(
   "/",
-   upload.single("image"),
+   upload.single("image"), 
+    (req, res, next) => { // This is a temporary middleware for debugging
+    console.log("--- DEBUGGING MULTER PARSING ---");
+    console.log("req.file:", req.file); // Should contain details about the image
+    console.log("req.body:", req.body); // Should contain lotName and occupied
+    console.log("---------------------------------");}
   validate(schema.parkingSchema, "body"), 
   lotController.lotpost
 );
